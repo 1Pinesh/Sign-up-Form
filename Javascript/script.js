fcf8448e76658f1d311969e9d8a609b908acc24b
1Pinesh <133708500+1Pinesh@users.noreply.github.com>
2023-08-16 22:24:34 +0100

    creating js funcntion to validate if password are the same

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,1 +1,13 @@
 console.log("hello world")
+
+// function to validate password to be the same
+
+function checkPassSame() {
+    let aPass = document.forms["signUpForm"]["pass"].value
+    let bPass = document.forms["signUpForm"]["conPass"].value
+
+    if (bPass !== aPass) {
+        alert("Password is not the same")
+        return false
+    }
+}
